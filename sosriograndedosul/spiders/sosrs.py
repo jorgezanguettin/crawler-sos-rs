1666ec951df21c7bc8e928f3b12c0e7436f1edd7
Jorge Zanguettin <jorgelzbr@gmail.com>
2024-05-14 17:00:20 +0100

    Add base spider

--- sosriograndedosul/spiders/sosrs.py
+++ sosriograndedosul/spiders/sosrs.py
@@ -3,8 +3,31 @@ import scrapy
 
 class SosrsSpider(scrapy.Spider):
     name = "sosrs"
-    allowed_domains = ["sos-rs.com"]
-    start_urls = ["https://sos-rs.com"]
+    domain = "https://api.sos-rs.com/"
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        yield from self.request_api()
+
+    def request_api(self, page=1):
+        yield scrapy.Request(
+            url=f"{self.domain}shelters?orderBy=updatedAt&order=desc&search=search%3D%26priority%3D&page={page}&perPage=50",
+            callback=self.parse_request_api,
+            meta={
+                "page": page
+            }
+        )
+
+    def parse_request_api(self, response):
+        data = response.json()
+
+        shelters = data["data"]["results"]
+
+        for shelter in shelters:
+            yield shelter
+
+        """
+        if shelters:
+            yield from self.request_api(
+                response.meta["page"] + 1
+            )
+        """
