cea3cfec2c2c0234726c933068545a22d1879959
Jorge Zanguettin <jorgelzbr@gmail.com>
2024-05-15 09:09:46 +0100

    Adicionado spider trigger (cloud functions)

---
+++ main.py
@@ -0,0 +1,16 @@
+import functions_framework
+from sosriograndedosul.spiders.sosrs import SosrsSpider
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+@functions_framework.http
+def lambda_handler(request):
+    settings = get_project_settings()
+
+    process = CrawlerProcess(settings)
+    process.crawl(SosrsSpider)
+    process.start()
+
+    return "Trigger Success"
