aaf5715fb3dd3eeac456464b1b6605df6395288d
daniel.mendes <daniel.mendes@newconsoftware.com.br>
2024-05-20 10:35:32 -0300

    Removendo fakeMain

--- fakeMain.py
+++
@@ -1,9 +0,0 @@
-from sosriograndedosul.spiders.ajuders import AjudeRSSpider
-from scrapy.crawler import CrawlerProcess
-from sosriograndedosul.pipelines import SosriograndedosulPipeline
-from scrapy.utils.project import get_project_settings
-
-settings = get_project_settings()
-process = CrawlerProcess(settings)
-process.crawl(AjudeRSSpider)
-process.start()
